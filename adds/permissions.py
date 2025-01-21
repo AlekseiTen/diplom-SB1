436db01138f06161fe61a6ba898c756286dd52d2
Aleksei Ten <amailcn@mail.ru>
2025-01-21 10:15:44 +0300

    удалил не нужные комментарии

--- adds/permissions.py
+++ adds/permissions.py
@@ -1,4 +1,4 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+from rest_framework.permissions import BasePermission
 
 
 class IsAdminOrOwnerOrReadOnly(BasePermission):
@@ -6,12 +6,9 @@ class IsAdminOrOwnerOrReadOnly(BasePermission):
     Разрешает редактировать и удалять объявление только:
     - Администраторам (могут редактировать/удалять любое объявление).
     - Владельцам (могут редактировать/удалять только свои объявления).
-    Просмотр доступен всем.
     """
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:  # Безопасные методы (GET, HEAD, OPTIONS)
-            return True
 
         # Администратор имеет доступ ко всем операциям
         if request.user.role == "admin":
